0955d5cce518277cc48caab3aeba5f5285f29169
yathin176 <68214590+yathin176@users.noreply.github.com>
2020-07-13 11:22:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCdDI4NVRl29WfTHZ8fiyYvKnjsgSfXPOU",
+  authDomain: "hacking-34c3a.firebaseapp.com",
+  databaseURL: "https://hacking-34c3a.firebaseio.com",
+  projectId: "hacking-34c3a",
+  storageBucket: "hacking-34c3a.appspot.com",
+  messagingSenderId: "571276777775",
+  appId: "1:571276777775:web:e157cc9bd4e8cb52f0e9da",
+  measurementId: "G-15YKRQCGKC"
 };
 
      
